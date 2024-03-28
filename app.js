212bb49546decb5dd824756228b2ebc217e6002d
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-28 17:47:11 +0100

    Connexion de la DB postgresql

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const fs = require('fs');
+const pg = require('pg');
+const url = require('url');
+require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
 
 const app = express()
+console.log(process.env.PORT)
 const port = process.env.PORT || 3000
 
 app.use(({res}) => {
@@ -9,3 +18,31 @@ res.status(404).json({message})
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
+
+const config = {
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DB,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync("./ca.pem").toString(),
+    },
+};
+
+const client = new pg.Client(config);
+client.connect(function (err) {
+    if (err)
+        throw err;
+    client.query("SELECT VERSION()", [], function (err, result) {
+        if (err)
+            throw err;
+
+        console.log(result.rows[0].version);
+        client.end(function (err) {
+            if (err)
+                throw err;
+        });
+    });
+});
