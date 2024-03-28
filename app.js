fe4201ddf65737f90665726e0415d83aca028235
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-28 17:57:35 +0100

    On prépare Prisma

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const express = require('express')
-const fs = require('fs');
-const pg = require('pg');
-const url = require('url');
 require('dotenv').config();
 
 if (process.env.NODE_ENV !== 'production') {
@@ -9,8 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
   }
 
 const app = express()
-console.log(process.env.PORT)
-const port = process.env.PORT || 3000
+const port = 3000
 
 app.use(({res}) => {
 const message = 'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.'
@@ -18,31 +14,3 @@ res.status(404).json({message})
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
-
-const config = {
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    database: process.env.DB,
-    ssl: {
-        rejectUnauthorized: true,
-        ca: fs.readFileSync("./ca.pem").toString(),
-    },
-};
-
-const client = new pg.Client(config);
-client.connect(function (err) {
-    if (err)
-        throw err;
-    client.query("SELECT VERSION()", [], function (err, result) {
-        if (err)
-            throw err;
-
-        console.log(result.rows[0].version);
-        client.end(function (err) {
-            if (err)
-                throw err;
-        });
-    });
-});
